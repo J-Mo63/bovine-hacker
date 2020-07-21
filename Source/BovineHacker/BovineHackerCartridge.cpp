442eb8c942d116ebe5f95b7a6fc152259a184ea1
Jonathan Moallem <jdmoallem@gmail.com>
2020-07-21 23:22:55 +1000

    Fixed Unreal Engine macro reflection issue and downgraded to Unreal version 4.24.3

--- Source/BovineHacker/BovineHackerCartridge.cpp
+++ Source/BovineHacker/BovineHackerCartridge.cpp
@@ -4,9 +4,13 @@
 void UBovineHackerCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    PrintLine(TEXT("Ok, good you're here.\nWe have to hack into these cows come on!"));
+    PrintLine(TEXT("Come on, hit enter, quick!"));
 }
 
 void UBovineHackerCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-
+    ClearScreen();
+    FString HiddenWord = TEXT("cake");
+    PrintLine(Input);
 }
